815a4014bcbdba7d829c374289aee2fb78d3d755
Malie Bertram <maliejobe@outlook.de>
2020-05-07 14:58:46 +0200

    --large field class bug fix

--- app.js
+++ app.js
@@ -8,13 +8,11 @@ function getTextWidth(text, font) {
     return Math.round(metrics.width);
 };
 
-console.log(getTextWidth("by Malie Bertram!", "13px arial"));  // close to 102
-
 function copyTitle() {
-    var titleInput = document.getElementById("claneo-title-input").value;  
-    document.getElementById("claneo-output-title").textContent = titleInput;
-    //console.log("Title: " + getTextWidth(document.getElementById("claneo-output-title").textContent, "18px arial"));
-    var titleOutput = getTextWidth(document.getElementById("claneo-output-title").textContent, "18px arial");
+    var titleInput = document.getElementById("title-input").value;  
+    document.getElementById("output-title").textContent = titleInput;
+    //console.log("Title: " + getTextWidth(document.getElementById("output-title").textContent, "18px arial"));
+    var titleOutput = getTextWidth(document.getElementById("output-title").textContent, "18px arial");
     document.getElementById("title-counter").textContent = titleOutput + "px";
     document.getElementById("title-char-count").textContent = titleInput.length + " Zeichen";
     
@@ -25,14 +23,14 @@ function copyTitle() {
         document.getElementById("title-counter").style.backgroundColor = "#006621";
     }
     if (titleOutput === 0) {
-        document.getElementById("claneo-output-title").textContent = "CLANEO: SEO, SEA und Content Marketing Agentur aus Berlin";
+        document.getElementById("output-title").textContent = "CLANEO: SEO, SEA und Content Marketing Agentur aus Berlin";
     }
 };
 function copyDesc() {
-    var descInput = document.getElementById("claneo-description-input").value;  
-    document.getElementById("claneo-output-description").textContent = descInput;
-    //console.log("Description: " + getTextWidth(document.getElementById("claneo-output-description").textContent, "13px arial"));
-    var descOutput = getTextWidth(document.getElementById("claneo-output-description").textContent, "13px arial");
+    var descInput = document.getElementById("description-input").value;  
+    document.getElementById("output-description").textContent = descInput;
+    //console.log("Description: " + getTextWidth(document.getElementById("output-description").textContent, "13px arial"));
+    var descOutput = getTextWidth(document.getElementById("output-description").textContent, "13px arial");
     document.getElementById("description-counter").textContent = descOutput + "px";
     document.getElementById("desc-char-count").textContent = descInput.length+ " Zeichen";
 
@@ -43,15 +41,15 @@ function copyDesc() {
         document.getElementById("description-counter").style.backgroundColor = "#006621";
     }
     if (descOutput === 0) {
-        document.getElementById("claneo-output-description").textContent = "Search-, Content- und Performance-Marketing aus erster Hand: Ein Team aus langjährigen Inhouse- und Agentur-Mitarbeitern. Jetzt Termin vereinbaren!";
+        document.getElementById("output-description").textContent = "Search-, Content- und Performance-Marketing aus erster Hand: Ein Team aus langjährigen Inhouse- und Agentur-Mitarbeitern. Jetzt Termin vereinbaren!";
     }
 };
 function copyUrl() {
-    var urlInput = document.getElementById("claneo-url-input").value;
-    document.getElementById("claneo-output-url").textContent = urlInput;
+    var urlInput = document.getElementById("url-input").value;
+    document.getElementById("output-url").textContent = urlInput;
     
 
     if (urlInput == '') {
-        document.getElementById("claneo-output-url").textContent = "www.claneo.com > snippet-optimizer";
+        document.getElementById("output-url").textContent = "www.claneo.com > snippet-optimizer";
     }
 };
